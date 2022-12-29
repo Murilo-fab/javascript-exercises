5c23805ab7ebf3beb2ca560a7f4e564888921507
Murilo <murilox26@hotmail.com.br>
2022-12-29 01:07:37 -0300

    Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(one, two) {
+    if (typeof(one) != "number" || typeof(two) != "number") {
+        return "ERROR"
+    } else if (one >= 0 && two >= 0) {
+        if (one < two)
+            return (one + two)*(two - one + 1)/2;
+        else
+            return (one + two)*(one - two + 1)/2;
+    } else
+        return "ERROR"
 };
 
 // Do not edit below this line
