e2e2f35efd27ad6f18057aa215b35d764e32a292
Murilo <murilox26@hotmail.com.br>
2022-12-28 23:43:31 -0300

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
+    let result = Array.from(arguments[0]);
 
+    for (let i = 1; i < arguments.length; i++) {
+        let id = result.indexOf(arguments[i]);
+        if ( id > -1) {
+            result.splice(id, 1);
+        }
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
